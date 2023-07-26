315bd38c2a641b8705aa211a540e92156336e5ca
GustaCortez <melvincortey@gmail.com>
2023-07-26 03:39:27 -0600

    listos

--- src/Personaje/Elfo.java
+++ src/Personaje/Elfo.java
@@ -3,18 +3,4 @@ package Personaje;
 public class Elfo extends Personaje implements Heroe{
 
 
-    @Override
-    public void atacar() {
-
-    }
-
-    @Override
-    public void defender() {
-
-    }
-
-    @Override
-    public void recibirAtaque() {
-
-    }
 }
